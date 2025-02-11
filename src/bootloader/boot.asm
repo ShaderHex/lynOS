2cf4d779c99c9d7f0f2b145d41aaee68adb81435
ShaderHex <barkott9@gmail.com>
2025-02-11 22:22:44 +0300

    Buffer the first letter from input and display

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,15 +1,11 @@
-mov ah, 0x0e
-mov al, 65
+[org 0x7c00]
+
+mov ah, 0x00
+int 0x16
+
+mov ah, 0x0E
+mov bh, 0
 int 0x10
-
-loop:
-    inc al
-    cmp al, 'Z' + 1
-    je exit
-    int 0x10
-    jmp loop
-
-exit:
-    jmp $
+jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
